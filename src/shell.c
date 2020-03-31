8e761371c9c66000ce58ea0e071dba1468cf5d6e
_ipal <malkoleyplay@gmail.com>
2020-03-31 18:47:16 +0300

    Convert tokenized input to simplest data structure for compability with exec(3)

--- src/shell.c
+++ src/shell.c
@@ -5,7 +5,7 @@ static inline void	shell_refresh_global_data(void) {
 }
 
 static inline void	cq_free(struct command_queue *restrict cq) {
-	for (size_t i = 0; cq->size >= i && cq->cmd[i]; ++i) {
+	for (size_t i = 0; cq->size > i && cq->cmd[i]; ++i) {
 		if (cq->cmd[i]->argv) {
 			for (size_t j = 0; cq->cmd[i]->argc >= j; j++)
 				free(cq->cmd[i]->argv[j]);
@@ -13,19 +13,49 @@ static inline void	cq_free(struct command_queue *restrict cq) {
 		}
 		free(cq->cmd[i]);
 	}
-	free((void*)cq);
+	free(cq);
 }
 
-static inline struct command_queue
-*tokens_to_cq(dll_t *restrict tokens, struct command_queue *restrict cq) {
-	(void)tokens;
-	(void)cq;
-	return NULL;
+static inline struct command_queue	*tokens_to_cq(dll_t *restrict tokens) {
+	static dll_obj_t *restrict	last_obj;
+	struct command_queue	*cq;
+	struct command *restrict	icmd = NULL;
+	dll_obj_t *restrict iobj =
+		((!tokens) ? last_obj : (last_obj = tokens->head));
+
+	if (!iobj)
+		return NULL;
+	assert(cq = calloc(1, sizeof(*cq)));
+	cq->size = 1;
+	cq->type = CQ_DEFAULT;
+	assert(cq->cmd = calloc(cq->size, sizeof(*cq->cmd)));
+	while (iobj) {
+		struct s_token_key *restrict	k = dll_getdata(iobj);
+		if (TK_PIPE == k->type) {
+			cq->type = CQ_PIPE;
+			++cq->size;
+			last_obj = iobj = iobj->next;
+			continue ;
+		} else if (TK_MULTI_CMD == k->type) {
+			last_obj = iobj->next;
+			break ;
+		}
+		if (!cq->cmd[cq->size - 1]) {
+			assert(icmd = cq->cmd[cq->size - 1] =
+				calloc(cq->size, sizeof(icmd)));
+		}
+		assert(icmd->argv = realloc(icmd->argv,
+			sizeof(*icmd->argv) * icmd->argc + 2));
+		assert(icmd->argv[icmd->argc++] = strdup(k->str));
+		icmd->argv[icmd->argc] = NULL;
+		last_obj = iobj = iobj->next;
+	};
+	return cq;
 }
 
 static int	print_token(const void *restrict data) {
 	static const char	*tk_str_types[] = {
-		"EXEC", "OPT", "ARG", "PIPE", "REDIR", "REDIRA", "REDIRD", NULL
+		"EXEC", "OPT", "ARG", "PIPE", "REDIR", "REDIRA", "REDIRD", "SEMI"
 	};
 	const struct s_token_key *restrict	tk = data;
 
@@ -51,12 +81,14 @@ void	shell(void) {
 			continue ;
 		}
 
-		dll_print(tokens, print_token);
-		continue ;
+		if (g_opt_dbg_level)
+			dll_print(tokens, print_token);
 
-		while ((cq = tokens_to_cq(tokens, cq))) {
+		cq = tokens_to_cq(tokens);
+		while (cq) {
 			cmd_run(cq);
 			cq_free(cq);
+			cq = tokens_to_cq(NULL);
 		}
 		dll_free(tokens);
 	}
